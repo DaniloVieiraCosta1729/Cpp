9f007244f6c488f836cb8eaa51fc3fcee24ca267
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-05-05 14:58:52 -0300

    Explorando variáveis.

--- ep1/helloWorld/helloWorld/Main.cpp
+++ ep1/helloWorld/helloWorld/Main.cpp
@@ -6,14 +6,37 @@ int multiplicar(int a, int b);
 
 int main()
 {
-	//mostra("Hello, World.");
+	mostra("Hello, World.");
 
-	int numero;
-	numero = std::cin.get();
+	//int numero;
+	//numero = std::cin.get();
 
-	std::cout << numero << std::endl; // caracter ASCII
+	//std::cout << numero << std::endl; // caracter ASCII
 
 	//std::cout << multiplicar(2, numero);
+
+	int a = 'A';
+
+	std::cout << a << std::endl;
+
+	std::cout << sizeof(short) << std::endl;
+
+
+	short x = 32736;
+	short z = x;
+	const short* i = &x;
+	short y = x + 500; // resultado: -32300, porque 32736 + 500 = 33236 congruente à -32300 (mod 2^16)
+
+	std::cout << y << std::endl;
+	std::cout << *i << std::endl;
+	std::cout << z << std::endl;
+
+	x = 2;
+
+	std::cout << z << std::endl;
+	std::cout << *i << std::endl;
+
+	
 
 	return 0;
 }
--- ep1/helloWorld/helloWorld/Mostra.cpp
+++ ep1/helloWorld/helloWorld/Mostra.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void mostrar(const char* texto)
+void mostra(const char* texto)
 {
 	std::cout << texto << std::endl;
 }
